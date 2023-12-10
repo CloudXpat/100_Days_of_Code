0032cf2610cb019101dda249a6486b4b51780aa5
CloudXpat <132170434+CloudXpat@users.noreply.github.com>
2023-12-10 22:39:20 +0000

    The Python Input Function

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 # Write your code below this line 👇
-print ("Day 1 - String Manipulation")
-print ('String Concatenation is done with the "+" sign.')
-print ('e.g. print("Hello " + "world")')
-print ("New lines can be created with a backlash and n.")
+input ("what is your name?")
+print ("Hello " + input ("what is your name?"))
